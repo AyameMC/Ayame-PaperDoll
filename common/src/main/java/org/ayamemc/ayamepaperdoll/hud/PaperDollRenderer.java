a75c0bbbbe5e04312c176b44f4d273c027ff3bfe
LightgraycaAT <15373262685@163.com>
2026-06-17 17:13:42 +0800

    26.2

--- common/src/main/java/org/ayamemc/ayamepaperdoll/hud/PaperDollRenderer.java
+++ common/src/main/java/org/ayamemc/ayamepaperdoll/hud/PaperDollRenderer.java
@@ -293,7 +293,7 @@ public class PaperDollRenderer {
         }
 
         // different renderState, offset, pose; same rotation, pos, size
-        ((GuiGraphicsExtractorInterface)graphics).addPicturesInPictureState(
+        ((GuiGraphicsExtractorInterface)graphics).ayame_paperDoll$addPicturesInPictureState(
                 new ModRenderState(
                         target,
                         offset,
